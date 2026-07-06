f047e52360ad4c77fbdbf89ce4cec11247cb34ee
Kaushal Tanna <kaushaltanna2007@gmail.com>
2026-07-06 11:49:15 +0530

    Time: 0 ms (100%), Space: 11.5 MB (86.98%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        
+        return vector<int>(digits.size() + 1, 0) , digits.insert(digits.begin(), 1), digits;
+    }
+};
