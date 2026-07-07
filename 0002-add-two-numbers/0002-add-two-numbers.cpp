9730e0237dfe6e827bbfd740a3347a18de0b48cb
Kaushal Tanna <kaushaltanna2007@gmail.com>
2026-07-07 16:41:45 +0530

    Time: 0 ms (100%), Space: 77.3 MB (12.58%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,16 +11,17 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummy = new ListNode(0);
-        ListNode* curr = dummy;
+        ListNode dummy(0);      // dummy head simplifies edge cases
+        ListNode* curr = &dummy;
         int carry = 0;
-        while (l1 || l2 || carry) {
+
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
             int sum = carry;
-            if (l1) {
+            if (l1 != nullptr) {
                 sum += l1->val;
                 l1 = l1->next;
             }
-            if (l2) {
+            if (l2 != nullptr) {
                 sum += l2->val;
                 l2 = l2->next;
             }
@@ -30,6 +31,6 @@ public:
             curr = curr->next;
         }
 
-        return dummy->next;
+        return dummy.next;
     }
 };
