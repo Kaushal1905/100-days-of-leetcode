f2611f871ef2d4bd3d597d245b10abe1bdf7be7c
Kaushal Tanna <kaushaltanna2007@gmail.com>
2026-07-16 19:22:32 +0530

    Time: 11 ms (15.48%), Space: 18.5 MB (86.81%) - LeetHub

--- 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.cpp
+++ 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     string makeSmallestPalindrome(string s) {
         int n = s.size();
-        char* p = s.data();      // raw pointer, avoids operator[] bounds-check overhead
         int i = 0, j = n - 1;
         
         while (i < j) {
-            char a = p[i], b = p[j];
-            if (a != b) {
-                char smaller = (a < b) ? a : b;  // avoid std::min call/template overhead
-                p[i] = smaller;
-                p[j] = smaller;
+            if (s[i] != s[j]) {
+                char smaller = min(s[i], s[j]);
+                s[i] = smaller;
+                s[j] = smaller;
             }
-            ++i;
-            --j;
+            i++;
+            j--;
         }
         
         return s;
