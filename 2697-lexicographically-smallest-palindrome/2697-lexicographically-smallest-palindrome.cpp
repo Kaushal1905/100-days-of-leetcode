fb501bf11baf3ceb79b191a1aa5e5815082d0355
Kaushal Tanna <kaushaltanna2007@gmail.com>
2026-07-16 19:23:04 +0530

    Time: 0 ms (100%), Space: 18.6 MB (60.97%) - LeetHub

--- 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.cpp
+++ 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.cpp
@@ -1,12 +1,18 @@
+#include <string>
+#include <algorithm>
+using namespace std;
+
 class Solution {
 public:
     string makeSmallestPalindrome(string s) {
-        int i = 0, j = (int)s.size() - 1;
+        int n = s.size();
+        int i = 0, j = n - 1;
         
         while (i < j) {
             if (s[i] != s[j]) {
                 char smaller = min(s[i], s[j]);
-                s[i] = s[j] = smaller;
+                s[i] = smaller;
+                s[j] = smaller;
             }
             i++;
             j--;
