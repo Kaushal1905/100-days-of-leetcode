0f6c80ad75529a62752cfd4c65d0a75108f5627e
Kaushal Tanna <kaushaltanna2007@gmail.com>
2026-09-12 16:47:07 +0530

    Time: 0 ms (100%), Space: 36.8 MB (72.34%) - LeetHub

---
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
+        vector<int> inDegree(n, 0);
+        for (int i = 0; i < n; i++) {
+            if (leftChild[i] != -1) {
+                inDegree[leftChild[i]]++;
+                if (inDegree[leftChild[i]] > 1) return false; 
+            }
+            if (rightChild[i] != -1) {
+                inDegree[rightChild[i]]++;
+                if (inDegree[rightChild[i]] > 1) return false; 
+            }
+        }
+        int root = -1;
+        for (int i = 0; i < n; i++) {
+            if (inDegree[i] == 0) {
+                if (root != -1) return false; 
+                root = i;
+            }
+        }
+        if (root == -1) return false; 
+        vector<bool> visited(n, false);
+        queue<int> q;
+        q.push(root);
+        visited[root] = true;
+        int count = 1;
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            if (leftChild[node] != -1) {
+                if (visited[leftChild[node]]) return false; 
+                visited[leftChild[node]] = true;
+                q.push(leftChild[node]);
+                count++;
+            }
+            if (rightChild[node] != -1) {
+                if (visited[rightChild[node]]) return false;
+                visited[rightChild[node]] = true;
+                q.push(rightChild[node]);
+                count++;
+            }
+        }
+        return count == n; 
+    }
+};
