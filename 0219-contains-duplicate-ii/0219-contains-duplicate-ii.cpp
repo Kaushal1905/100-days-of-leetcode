aa1798dad3d40fa2295ba779754262b6125cde05
Kaushal Tanna <kaushaltanna2007@gmail.com>
2026-09-19 18:12:32 +0530

    Time: 135 ms (14.92%), Space: 129.5 MB (51.71%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -3,7 +3,6 @@ public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         unordered_set<int> window;
         window.reserve(min((size_t)k + 1, nums.size()));
-
         for (int i = 0; i < nums.size(); i++) {
             if (i > k) {
                 window.erase(nums[i - k - 1]);   
